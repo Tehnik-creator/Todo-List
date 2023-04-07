40ee74253e5c4b2b2e49226212bc8c7c3ccc2f44
Tehnik-creator <maksimosipov_2000@mail.ru>
2023-04-07 10:49:43 +0300

    #3

---
+++ index.js
@@ -0,0 +1,54 @@
+const addBtn = document.querySelector('.task_add_btn')
+const addInput = document.querySelector('.add_task_input')
+
+const allTasks = [1, 2];
+
+const STATUSES = {
+    TODO: 'TODO',
+    IN_PROGRESS: 'IN_PROGRESS',
+    DONE: 'DONE',
+};
+
+const STATES = {
+    ACTIVE: 'ACTIVE',
+    ARCHIVE: 'ARCHIVE',
+    DELETED: 'DELETED'
+};
+// false true
+// Boolean(), String(), Number()
+console.log(addInput.value)
+if (addInput.value === '') {
+    addInput.classList.add("is-invalid");
+} else {
+    addInput.classList.remove("is-invalid");
+}
+
+
+function addTask(event) {
+    event.preventDefault();
+    const label = addInput.value;
+    if (label) {
+        addInput.classList.add("is-invalid");
+    }
+
+    if (addInput.value === '') {
+        addInput.classList.add("is-invalid");
+    } else {
+        addInput.classList.remove("is-invalid");
+    }
+
+    if (!label) return;
+
+    const task = {
+        id: crypto.randomUUID(),
+        label:  label,
+        createdAt: new Date(),
+        status: STATUSES.TODO,
+        state: STATES.ACTIVE
+    }
+    allTasks.push(task);
+    console.log(allTasks)
+}
+
+addBtn.addEventListener('click', addTask)
+
