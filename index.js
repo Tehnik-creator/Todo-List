d55e89eafb4ff3a166dd63ac1dc0630163f0d604
Tehnik-creator <maksimosipov_2000@mail.ru>
2023-04-22 11:59:12 +0300

    feat: [#5] add task design
    feat: [#5] add task design

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const addBtnSubmit = document.querySelector('.task_add_btn')
 const addInput = document.querySelector('.add_task_input')
+const taskList = document.querySelector('.task_list')
 
 const allTasks = [];
 
@@ -38,7 +39,49 @@ function addTask(event) {
     }
 
     allTasks.push(task);
-    console.log(allTasks)
+    console.log(allTasks);
+
+    addInput.value = "";
+
+    renderTasks(allTasks)
+}
+
+function renderTasks(tasks) {
+    const card = document.createElement("div")
+    const cardBody = document.createElement("div")
+    const cardActions = document.createElement("div")
+    const actionCheckbox = document.createElement("input")
+    const iconEdit = document.createElement("i")
+    const iconDelete = document.createElement("i")
+    const actionEdit = document.createElement("button")
+    const actionDelete = document.createElement("button")
+    const label = document.createElement("strong")
+
+    cardBody.classList.add("card-body");
+    cardBody.classList.add("d-flex", "flex-row", "justify-content-between", "align-items-center");
+    cardActions.classList.add("card-actions", "d-flex", "flex-row", "gap-3");
+    actionEdit.classList.add("btn", "btn-light");
+    actionDelete.classList.add("btn", "btn-light");
+    actionCheckbox.type = "checkbox";
+    actionCheckbox.classList.add("form-check-input");
+    iconEdit.classList.add("bi", "bi-pencil-square", "d-flex", "icons");
+    iconDelete.classList.add("bi", "bi-trash", "d-flex", "icons");
+    card.classList.add("card", "mb-4",);
+
+    actionEdit.appendChild(iconEdit);
+    actionDelete.appendChild(iconDelete);
+    card.appendChild(cardBody);
+    cardActions.appendChild(actionCheckbox);
+    cardActions.appendChild(actionEdit);
+    cardActions.appendChild(actionDelete);
+    cardBody.appendChild(label);
+    cardBody.appendChild(cardActions);
+
+    tasks.forEach((task) => {
+        label.innerText = task.label
+        taskList.appendChild(card)
+    })
+
 }
 
 addBtnSubmit.addEventListener('click', addTask)
